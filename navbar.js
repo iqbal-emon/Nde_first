c2cc82a0ee5aa369def021016a1f98823517fa77
Emon <ihemon281@gmail.com>
2023-11-13 21:17:41 +0600

    late night

--- navbar.js
+++ navbar.js
@@ -16,22 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
             <button onclick="topFunction()" id="myBtn" title="Go to top"><img src="./image/card/arrow.png" alt="" width="15" height="15"></button>
         </nav>
         <div class="Carousel" id="ca">
-        // <img class="img1" src="./image/caro1.jpg" width="100%" height="600" alt="">
-        // <div class="text-caro1">
-        //     <h2 class="allpro" id="monospace">New Inpsiration 2023</h2>
-        //     <h1 class="word-animation">
-        //         <span>CLOTHING</span>
-        //         <span>MADE</span>
-        //         <span>FOR</span>
-        //         <span>YOU!</span>
-        //     </h1>
-        //     <h3 class="allpro" id="monospace">Trending from men and women style collection</h3>
+        <img class="img1" src="./image/caro1.jpg" width="100%" height="600" alt="">
+        <div class="text-caro1">
+            <h2 class="allpro" id="monospace">New Inpsiration 2023</h2>
+            <h1 class="word-animation">
+                <span>CLOTHING</span>
+                <span>MADE</span>
+                <span>FOR</span>
+                <span>YOU!</span>
+            </h1>
+            <h3 class="allpro" id="monospace">Trending from men and women style collection</h3>
 
-        //     <div class="btn">
-        //         <button class="btn1">SHOP WOMEN'S</button>
-        //         <button class="btn2">SHOP MEN'S</button>
-        //     </div>
-        // </div>
+            <div class="btn">
+                <button class="btn1">SHOP WOMEN'S</button>
+                <button class="btn2">SHOP MEN'S</button>
+            </div>
+        </div>
   
       </div>  
  `;
